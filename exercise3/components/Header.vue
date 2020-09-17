b83f34e8532af34d37ebc5c2d17e5af5ac0d8622
Антонов Андрей <an_42@mail.ru>
2020-09-17 20:16:11 +0700

    добавлен Header в default layout

---
+++ exercise3/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav>
+    <div class="header-title">
+      Магазин
+    </div>
+    <div class="header-cart">
+      <IconButton
+        :color="'#fff'"
+        :hoverColor="'#fff'"
+        :class="'flex-wrap'"
+        :style="{marginRight: '5px'}"
+      >
+        <template slot-scope="props">
+          <div
+            :style="{color: props.color, marginRight: '5px'}"
+          >Корзина</div>
+          <CartIcon
+            :color="props.color" :size="25"
+            :style="{marginRight: '5px'}"
+          />
+          <CircleIcon
+            :symbol="0"
+            :style="{background: props.color, color: '#3b8070'}"
+          />
+        </template>
+      </IconButton>
+    </div>
+  </nav>
+</template>
+
+<script>
+  import IconButton from "./IconButton";
+  import CartIcon from "./Icons/CartIcon";
+  import CircleIcon from "./CircleIcon";
+  export default {
+    name: "Header",
+    components: {CircleIcon, CartIcon, IconButton}
+  }
+</script>
+
+<style scoped>
+  nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background: #3b8070;
+    padding: 10px 20px;
+  }
+  .header-title {
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    color: #fff;
+  }
+  .header-cart {
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    color: #fff;
+  }
+</style>
---
+++ exercise3/components/IconButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+      class="icon-button"
+      v-on:mouseover="hover=true"
+      v-on:mouseout="hover=false"
+      v-on:click="($event) => {
+        $event.stopPropagation();
+        action();
+      }"
+  >
+    <slot :color="hover ? hoverColor : color"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "IconButton",
+    props: {
+      color: String,
+      hoverColor: String,
+      action: {
+        default: ()=>{},
+        type: Function
+      },
+    },
+    data() {
+      return {
+        hover: false,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .icon-button {
+    cursor: pointer;
+  }
+</style>
